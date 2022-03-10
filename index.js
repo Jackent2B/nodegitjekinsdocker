3fce2a3f5b75e2a7ca0f04a9feb278366e6b369d
jackent2b <jayant.khandelwal7@gmail.com>
2022-03-10 18:04:41 +0530

    second commit for jenkins demo

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get('/details',(req,res)=>{
     res.send("This is details page")
 })
 
+app.get('/team',(req,res)=>{
+    res.send("This is our team")
+})
+
+
 app.listen(3000,(req,res)=>{
     console.log("Server has started")
 })
+
